78305ceca6769399fe92c97d2f90b47ce7591a2c
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-09 21:03:06 +0300

    Робота готова без (state)

--- src/redux/contacts/contacts-reducers.js
+++ src/redux/contacts/contacts-reducers.js
@@ -18,15 +18,15 @@ const filter = createReducer('', {
 });
 
 const loading = createReducer(false, {
-  [fetchContacts.pending]: (state, { payload }) => true,
-  [fetchContacts.fulfilled]: (state, { payload }) => false,
-  [fetchContacts.rejected]: (state, { payload }) => false,
-  [fetchAddContact.pending]: (state, { payload }) => true,
-  [fetchAddContact.fulfilled]: (state, { payload }) => false,
-  [fetchAddContact.rejected]: (state, { payload }) => false,
-  [fetchDeleteContact.pending]: (state, { payload }) => true,
-  [fetchDeleteContact.fulfilled]: (state, { payload }) => false,
-  [fetchDeleteContact.rejected]: (state, { payload }) => false,
+  [fetchContacts.pending]: () => true,
+  [fetchContacts.fulfilled]: () => false,
+  [fetchContacts.rejected]: () => false,
+  [fetchAddContact.pending]: () => true,
+  [fetchAddContact.fulfilled]: () => false,
+  [fetchAddContact.rejected]: () => false,
+  [fetchDeleteContact.pending]: () => true,
+  [fetchDeleteContact.fulfilled]: () => false,
+  [fetchDeleteContact.rejected]: () => false,
 });
 
 export const rootReducer = combineReducers({
